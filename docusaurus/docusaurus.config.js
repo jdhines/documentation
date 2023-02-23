5a27649d84f263662741c49b82093e38c7afdb94
Udi Silvestre <udi.silvestre@strapi.io>
2023-02-23 18:39:14 -0300

    internal: styles with sass - wip

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -87,7 +87,7 @@ const config = {
         },
         blog: false, // we're using docs-only mode for now — see https://docusaurus.io/docs/docs-introduction
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve('./src/scss/__index.scss'),
         },
       }),
     ],
@@ -226,6 +226,7 @@ const config = {
         containerId: 'GTM-KN9JRWG',
       },
     ],
+    'docusaurus-plugin-sass',
     // 'plugin-image-zoom', // temp. disabled while we fix the bug
     /**
      * Seems like we have an issue where a medium-zoom--hidden class is applied on the second, top-most (z-index wise) image,
